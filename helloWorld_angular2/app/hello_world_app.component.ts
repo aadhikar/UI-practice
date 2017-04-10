69c2aecd2829be53c705f526fc4507e7c3f9e061
Anil <aadhikar@users.noreply.github.com>
2017-04-10 15:38:51 -0500

    included template and styles

--- helloWorld_angular2/app/hello_world_app.component.ts
+++ helloWorld_angular2/app/hello_world_app.component.ts
@@ -2,14 +2,8 @@ import {Component, View} from "angular2/core";
 
 @Component({
    selector: 'my-app',
-   template: `
-    <style>
-    h1 {
-      color: purple;
-    }
-    </style>
-    <h1>Hello World !!</h1>
-    `
+   styleUrls: ['app/helloWorld.css'],
+   templateUrl: './app/helloWorld.html'
 })
 
 
