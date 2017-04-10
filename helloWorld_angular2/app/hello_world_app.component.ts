8d8d5c251ef0f99ec13023e9e448f39726073688
Anil <aadhikar@users.noreply.github.com>
2017-04-10 15:28:07 -0500

    helloWorld_angular2
    Tutorial_2 helloWorld_angular2

---
+++ helloWorld_angular2/app/hello_world_app.component.ts
@@ -0,0 +1,18 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app',
+   template: `
+    <style>
+    h1 {
+      color: purple;
+    }
+    </style>
+    <h1>Hello World !!</h1>
+    `
+})
+
+
+export class MyHelloWorldClass {
+
+}
