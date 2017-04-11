fb2af6f4dacdcedfa35f38c2c50924c4620b6779
Anil <aadhikar@users.noreply.github.com>
2017-04-11 15:24:29 -0500

    turorial6_metadata_angular2
    metadata_angular2

---
+++ metadata_angular2/app/metadata_main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from "angular2/platform/browser"    //importing bootstrap function
+import {MyMetadata} from "./metadata_app.component"    //importing component function
+
+bootstrap(MyMetadata);
