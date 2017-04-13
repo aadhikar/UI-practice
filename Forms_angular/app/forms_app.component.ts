18318edc2aa68370412bf2a2288d07d9db166edf
Anil <aadhikar@users.noreply.github.com>
2017-04-13 16:07:40 -0500

    added template
    added template

--- Forms_angular/app/forms_app.component.ts
+++ Forms_angular/app/forms_app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 import {ContactComponent} from './contact-form.component'
 @Component({
 	selector: 'my-app',
-	template: '',
+	template: '<contact-form></contact-form>',
 	directives: [ContactComponent]
 })
 export class AppComponent { }
