4144737c69429089d33b3ca2f9dd03ebb7dfba15
Anil <aadhikar@users.noreply.github.com>
2017-04-13 11:27:43 -0500

    tutorial12_KeyEventFiltering_angular2
    Angular 2 - Key Event Filtering

---
+++ KeyEventFiltering_angular2/app/event_filtering.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'event-filtering',
+  templateUrl: './app/event_filtering.component.html',
+  styleUrls: ['./app/event_filtering.component.css']
+})
+export class EventFilteringComponent {
+  values='';
+}
---
+++ KeyEventFiltering_angular2/system.config.js
@@ -0,0 +1,18 @@
+System.config({
+		transpiler: 'typescript',
+		typescriptOptions: 
+						{ 
+							emitDecoratorMetadata: true 
+						},
+		packages: 
+				{'app': 
+					{
+						defaultExtension: 'ts'
+					}
+				},
+		map: 
+			{ 
+				'app': './app' 
+			}
+	});
+System.import('app/user_input_event_filtering').then(null, console.error.bind(console));
