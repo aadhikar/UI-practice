56e7ad617c28945a4c0b5406e5ac394edeb02f7e
Anil <aadhikar@users.noreply.github.com>
2017-04-13 11:39:03 -0500

    tutorial13_OnBlurEvent_angular2
    Angular 2 - On Blur Event

---
+++ OnBlurEvent_angular2/app/user_onblur_event.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'onblur-event',
+	templateUrl: './app/user_onblur_event.component.html',
+	styleUrls: ['./app/user_onblur_event.component.css']
+})
+export class OnblurEventComponent {
+	values='';
+}
